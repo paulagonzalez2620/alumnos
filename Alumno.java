46ac087d76ecf3d40fec3aa5649760b25838caf4
paulagonzalez2620 <127936084+paulagonzalez2620@users.noreply.github.com>
2023-06-16 13:17:44 +0200

    Add files via upload

---
+++ Alumno.java
@@ -0,0 +1,13 @@
+package Alumno;
+
+/**
+ * Alumno.java
+ * Definición de la clase Alumno
+ * @author Luis José Sánchez
+ */
+
+public class Alumno {
+  public String nombre;
+  public double notaMedia = 0.0;
+  
+}
